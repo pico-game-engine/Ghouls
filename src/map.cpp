1943f0f544bb94f45816ed4b1f5a4b0f3069f6e3
jblanked <82678820+jblanked@users.noreply.github.com>
2026-05-01 09:36:57 -0400

    void unused parameters

--- src/map.cpp
+++ src/map.cpp
@@ -10,6 +10,8 @@ bool mapPackLoadFromFile(const char *filename, map_data_t *outMapData)
     const size_t expectedSize = sizeof(map_data_t);
     return ENGINE_STORAGE_READ(filename, outMapData, expectedSize) == expectedSize;
 #else
+    (void)filename;
+    (void)outMapData;
     return false;
 #endif
 }
@@ -19,6 +21,8 @@ bool mapPackSaveToFile(const char *filename, const map_data_t *mapData)
 #if defined(ENGINE_STORAGE_INCLUDE) && defined(ENGINE_STORAGE_WRITE)
     return ENGINE_STORAGE_WRITE(filename, mapData, sizeof(map_data_t));
 #else
+    (void)filename;
+    (void)mapData;
     return false;
 #endif
 }
